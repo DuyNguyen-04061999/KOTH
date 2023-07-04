9b00c7020201f53ecf76de71fac81f7f5b2eb4f4
Vũ Thành. Đạt <vuthanhdat297@gmail.com>
2023-07-04 16:45:48 +0700

    fix homepage

--- src/pages/GameList/index.jsx
+++ src/pages/GameList/index.jsx
@@ -1,50 +1,40 @@
-import { Container, Grid } from "@mui/material";
+import { Button, Container } from "@mui/material";
 import "./index.scss";
 import Layout from "../../components/Layout";
+import { useDispatch } from "react-redux";
+import _socket from "../../redux-saga-middleware/config/socket";
 
 export default function GameList() {
+  const dispatch = useDispatch()
   
   const renderS = () => {
     return (
-      <div className="showgame">
+      <div className="showgame pt-5">
         <Container maxWidth={"lg"}>
-          <Grid container spacing={4}>
-            {renderAllgame}
-          </Grid>
+        <Button sx={{
+              background:"red"
+            }}
+              onClick={() => {
+                _socket.emit("createTournament")
+              }}
+            >1</Button>
+            <Button sx={{
+              background:"blue"
+            }}
+            onClick={() => {
+              _socket.emit("joinTournament")
+            }}
+            >2</Button>
+            <Button sx={{
+              background:"white"
+            }}>3</Button>
+            <Button sx={{
+              background:"yellow"
+            }}>4</Button>
         </Container>
       </div>
     );
   };
-
-  const renderAllgame = [].map((e, index) => {
-    return (
-      <Grid
-        item
-        xs={6}
-        sm={4}
-        key={index}
-        className="d-flex align-items-center justify-content-center"
-      >
-        <div className="new-list">
-          <div
-            id={e.id}
-            className="card-item"
-            onClick={() => {
-             
-            }}
-          >
-            <div
-              className="card-header"
-              style={{ height: "135px", border: "none", padding: "10px" }}
-            >
-              <img src={e.img} alt={e.name} />
-            </div>
-            {e.title && <img src={e.title} alt="..." className="theme-pvp" />}
-          </div>
-        </div>
-      </Grid>
-    );
-  });
 
   return <Layout children={renderS()} />;
 }
