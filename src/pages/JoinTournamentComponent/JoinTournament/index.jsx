eee9b661d362046e783b7e37ce72ab2ebd4b163a
Vũ Thành. Đạt <vuthanhdat297@gmail.com>
2023-08-24 14:01:12 +0700

    join game

--- src/pages/JoinTournamentComponent/JoinTournament/index.jsx
+++ src/pages/JoinTournamentComponent/JoinTournament/index.jsx
@@ -30,6 +30,7 @@ import InspirationTTF from "../../../assets/font/CynthoNextMedium.otf";
 import LeaderBoard from "../LeaderBoard";
 import DetailVoucher from "../DetailVoucher";
 import "./index.scss";
+import GameInTournament from "../GameInTournament";
 const theme = createTheme({
   typography: {
     fontFamily: "Cyntho Next",
@@ -701,7 +702,7 @@ export default function JoinTournament() {
                     {/* ----------------------- */}
                     <div className="cardWrap">
                       <div className="cardT cardLeft">
-                        <h5 style={{color:"#BE48ED"}}>SS Z-Flip 5 free voucher</h5>
+                        <h5 style={{color:"#BE48ED", marginTop:"15px"}}>SS Z-Flip 5 free voucher</h5>
                         <Grid container>
                           <Grid item md={5}>
                             <div className="title d-flex flex-column mb-2">
@@ -729,7 +730,7 @@ export default function JoinTournament() {
                           </Grid>
                         </Grid>
                       </div>
-                      <div className="card cardRight">
+                      <div className="cardT cardRight">
                         <div className="eye"></div>
                         <div className="number">
                           <img src={images.GameTournament} alt="..." width={100} height={105}  />
@@ -737,6 +738,12 @@ export default function JoinTournament() {
                       </div>
                     </div>
                     {/* --------------------------  */}
+                  </Box>
+                  <Box sx={{
+                    display:"flex",
+                    alignItems:"flex-start"
+                  }}>
+                  <GameInTournament />
                   </Box>
                   <Box>
                     <Box
