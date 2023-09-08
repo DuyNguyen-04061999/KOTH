4ea857c29d248ade3ec59f3e2500916fb3997ff1
thanhdat <thanhdat@gadgame.com>
2023-09-08 14:09:00 +0700

    fix: button close stripe

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -155,7 +155,6 @@ export default function Layout(props) {
       window.location.reload();
     }
   }, [router, startGameCheck]);
-  console.log(123);
 
   useEffect(() => {
     if (token && !router?.includes(`selectroom`)) {
