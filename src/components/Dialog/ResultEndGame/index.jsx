d356752e491bd0adf127f18d5c5cb39d416e1126
thanhduy <btduy12.11lvl@gmail.com>
2023-09-08 16:46:21 +0700

    feat: Add Playing Game Screen

--- src/components/Dialog/ResultEndGame/index.jsx
+++ src/components/Dialog/ResultEndGame/index.jsx
@@ -2,16 +2,18 @@ import * as React from "react";
 import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
 import { Box, Typography } from "@mui/material";
-import './index.scss'
+import "./index.scss";
 import { useDispatch, useSelector } from "react-redux";
 import { toggleOpenResultEndGame } from "../../../redux-saga-middleware/reducers/tournamentReducer";
 
 export default function ResultEndGame() {
-  const { endGameScore, isResultEndGame } = useSelector(state => state.tournamentReducer);
-  const dispatch = useDispatch()
+  const { endGameScore, isResultEndGame } = useSelector(
+    (state) => state.tournamentReducer
+  );
+  const dispatch = useDispatch();
   const handleClose = () => {
-    dispatch(toggleOpenResultEndGame())
-    window.location.reload()
+    dispatch(toggleOpenResultEndGame());
+    // window.location.reload()
   };
 
   return (
@@ -23,11 +25,11 @@ export default function ResultEndGame() {
         aria-describedby="alert-dialog-description"
         sx={{
           zIndex: 2000,
-            "& .MuiDialog-paper": {
-                borderRadius: 0,
-                backgroundColor:"transparent",
-                boxShadow:"none"
-              },
+          "& .MuiDialog-paper": {
+            borderRadius: 0,
+            backgroundColor: "transparent",
+            boxShadow: "none",
+          },
         }}
       >
         <Box
@@ -65,17 +67,24 @@ export default function ResultEndGame() {
                 {/* <p className="result">of 100</p> */}
               </div>
               <div className="result-text-box">
-                <div className="heading-secondary">excellent work, keep going</div>
+                <div className="heading-secondary">
+                  excellent work, keep going
+                </div>
                 {/* <p className="paragraph">
                   You scored higher than 65% of the people who have taken these
                   tests.
                 </p> */}
               </div>
               <div className="summary__cta">
-                <button onClick={() => {
-                  dispatch(toggleOpenResultEndGame())
-                  window.location.reload()
-                }} className="btnResult btn__continue">Continue</button>
+                <button
+                  onClick={() => {
+                    dispatch(toggleOpenResultEndGame());
+                    // window.location.reload();
+                  }}
+                  className="btnResult btn__continue"
+                >
+                  Continue
+                </button>
               </div>
             </div>
           </div>
