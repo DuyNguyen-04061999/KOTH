a6ed00807a4c9d4eda108db0aa217320e843a4c4
thanhduy <btduy12.11lvl@gmail.com>
2023-09-21 15:04:47 +0700

    feat: Video Game Screen

--- src/pages/JoinTournamentComponent/PlayGame/index.jsx
+++ src/pages/JoinTournamentComponent/PlayGame/index.jsx
@@ -8,11 +8,10 @@ import { imageDesktop, images, video } from "../../../utils/images";
 import { toggleOpenResultEndGame } from "../../../redux-saga-middleware/reducers/tournamentReducer";
 import useWindowDimensions from "../../../utils/useWindowDimensions";
 import _socket from "../../../redux-saga-middleware/config/socket";
-import ReactPlayer from "react-player";
 import GameInTournament from "../GameInTournament";
 import moment from "moment";
 import { sliceString } from "../../../utils/helper";
-import { useFullScreenHandle } from "react-full-screen";
+import VideoComponent from "./VideoComponent";
 
 export default function PlayGame(props) {
   const { detailTournament, setStartGame, videoGame, setVideoGame } = props;
@@ -29,16 +28,7 @@ export default function PlayGame(props) {
   useEffect(() => {
     setSocket(_socket);
   }, []);
-  useEffect(() => {
-    let timeInterval = setInterval(() => {
-      if (second > 0) {
-        setSeconds(second - 1);
-      }
-    }, 1000);
-    return () => {
-      clearInterval(timeInterval);
-    };
-  }, [second]);
+
   useEffect(() => {
     const handler = (res) => {
       setTimeout(() => {
@@ -123,138 +113,12 @@ export default function PlayGame(props) {
           }}
         >
           {detailTournament?.tournamentVideo && videoGame ? (
-            <Box
-              sx={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                height: "100%",
+            <VideoComponent
+              detailTournament={detailTournament}
+              setVideoGame={(data) => {
+                setVideoGame(data);
               }}
-            >
-              <Box
-                sx={{
-                  position: device === "Desktop" ? "relative" : "fixed",
-                  top: "0px",
-                  left: "0px",
-                }}
-              >
-                {" "}
-                {deviceType === "iOS" && device === "Mobile" ? (
-                  <video
-                    autoplay
-                    className={isFullScreen ? "fullscreenVideo" : ""}
-                    width={"100%"}
-                    playsInline
-                    // muted
-                    controls={deviceType === "iOS" ? true : false}
-                    onPlay={() => {
-                      setSeconds(7);
-                    }}
-                    onEnded={() => {
-                      setVideoGame(false);
-                      if (device === "Mobile" || device === "Tablet") {
-                        setIsFullScreen(true);
-                      }
-                    }}
-                  >
-                    <source
-                      src={
-                        detailTournament?.tournamentVideo
-                          ? process.env.REACT_APP_SOCKET_SERVER +
-                            "/" +
-                            detailTournament?.tournamentVideo
-                          : video.LogoAnim
-                      }
-                      type="video/mp4"
-                    />
-                  </video>
-                ) : (
-                  <ReactPlayer
-                    width="100%"
-                    height="100%"
-                    playsInline={true}
-                    controls={true}
-                    onStart={() => {
-                      setSeconds(7);
-                    }}
-                    onEnded={() => {
-                      setVideoGame(false);
-                      if (device === "Mobile" || device === "Tablet") {
-                        setIsFullScreen(true);
-                      }
-                      setSeconds(null);
-                    }}
-                    config={{
-                      file: {
-                        attributes: {
-                          controlsList: "nofullscreen",
-                          className: "video-player",
-                          "data-setup": { liveui: true, liveTracker: false },
-                        },
-                      },
-                    }}
-                    playing={true}
-                    url={
-                      detailTournament?.tournamentVideo
-                        ? process.env.REACT_APP_SOCKET_SERVER +
-                          "/" +
-                          detailTournament?.tournamentVideo
-                        : video.LogoAnim
-                    }
-                  />
-                )}
-                {second !== null && (
-                  <Box
-                    onClick={() => {
-                      if (second === 0) {
-                        setVideoGame(false);
-                        if (device === "Mobile" || device === "Tablet") {
-                          setIsFullScreen(true);
-                        }
-                        setSeconds(null);
-                      }
-                    }}
-                    sx={{
-                      position:
-                        device === "Desktop" ||
-                        ((device === "Mobile" || device === "Tablet") &&
-                          orientation === "portrait")
-                          ? "absolute"
-                          : "fixed",
-                      top: width < 576 ? "70%" : "80%",
-                      right: "20px",
-                      display: "flex",
-                      alignItems: "center",
-                      padding: "10px",
-                      backgroundColor: "#000",
-                      border: "2px solid #ffff",
-                      cursor: "pointer",
-                      zIndex: "1000000000000",
-                    }}
-                  >
-                    <Typography
-                      sx={{
-                        color: "white",
-                        marginRight: "4px",
-                        fontSize: width < 576 ? "10px !important" : "14px",
-                      }}
-                    >
-                      {second !== 0
-                        ? `You can skip Ads after ${second}s`
-                        : "Skip Ads"}
-                    </Typography>
-                    <i
-                      style={{
-                        color: "#ffff",
-                        marginTop: "2px",
-                        fontSize: width < 576 ? "10px" : "14px",
-                      }}
-                      className="fa-solid fa-angle-right"
-                    ></i>
-                  </Box>
-                )}
-              </Box>
-            </Box>
+            />
           ) : (
             <Box>
               <iframe
@@ -263,7 +127,7 @@ export default function PlayGame(props) {
                   width: "100%",
                   height:
                     device === "Mobile" || device === "Tablet"
-                      ? "100%"
+                      ? "100vh"
                       : "800px",
                 }}
                 title="Playgame"
