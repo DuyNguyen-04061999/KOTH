ae95f0ed0eaa46ec345d5a2cb8cce1544cc0fb73
thanhduy <btduy12.11lvl@gmail.com>
2023-10-05 16:39:11 +0700

    feat:change game

--- src/components/GameManager/UnityGameComponent.jsx
+++ src/components/GameManager/UnityGameComponent.jsx
@@ -179,7 +179,32 @@ export default function UnityGameComponent(props) {
       window.removeEventListener("popstate", onBeforeUnload);
     };
   }, [dispatch, unload, fmod]);
+  const [devicePixelRatio, setDevicePixelRatio] = useState(
+    window.devicePixelRatio
+  );
 
+  useEffect(
+    function () {
+      // A function which will update the device pixel ratio of the Unity
+      // Application to match the device pixel ratio of the browser.
+      const updateDevicePixelRatio = function () {
+        setDevicePixelRatio(window.devicePixelRatio);
+      };
+      // A media matcher which watches for changes in the device pixel ratio.
+      const mediaMatcher = window.matchMedia(
+        `screen and (resolution: ${devicePixelRatio}dppx)`
+      );
+      // Adding an event listener to the media matcher which will update the
+      // device pixel ratio of the Unity Application when the device pixel
+      // ratio changes.
+      mediaMatcher.addEventListener("change", updateDevicePixelRatio);
+      return function () {
+        // Removing the event listener when the component unmounts.
+        mediaMatcher.removeEventListener("change", updateDevicePixelRatio);
+      };
+    },
+    [devicePixelRatio]
+  );
   return (
     <Fragment>
       {!isLoaded && !videoGame && (
@@ -201,6 +226,7 @@ export default function UnityGameComponent(props) {
         }}
         unityProvider={unityProvider}
         ref={unityRef}
+        devicePixelRatio={devicePixelRatio}
       />
     </Fragment>
   );
