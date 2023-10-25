e58528c873a034040687fa8f786a0b407d3919ec
thanhduy <btduy12.11lvl@gmail.com>
2023-10-25 17:34:38 +0700

    fix: Fix Nav bar in ipad pro screen

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -126,7 +126,7 @@ export default function Layout(props) {
   // useEffect(() => {
   //   dispatch(setBadgeChat(false))
   // },[chatWorld])
-
+  console.log("device: ", device);
   useEffect(() => {
     if (
       router &&
@@ -512,7 +512,7 @@ export default function Layout(props) {
             sx={{
               transition: "visibility 0s, all 0.2s ease-in-out",
               position: "relative",
-              zIndex: width < 1200 ? "1024" : "0",
+              zIndex: "1024",
               // width: "400px !important",
               // "& .MuiGrid-item": {
               //   minWidth: "400px !important",
