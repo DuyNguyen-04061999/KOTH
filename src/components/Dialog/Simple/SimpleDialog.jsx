a64daf5b1bae1f40954613c698255b5918c969fc
Riskboy <lamdinh@gadgame.com>
2023-11-03 17:25:17 +0700

    fix: Update simple dialog

--- src/components/Dialog/Simple/SimpleDialog.jsx
+++ src/components/Dialog/Simple/SimpleDialog.jsx
@@ -4,43 +4,43 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { useDispatch, useSelector } from "react-redux";
 
-import { getAppType } from "../. ./. ./utils/helper";
-import { images, sign } from "../. ./. ./utils/images";
+import { getAppType } from "../../../utils/helper";
+import { images, sign } from "../../../utils/images";
 
 import {
   clickTab,
   toggleLoginDialog,
-} from "../. ./. ./redux-saga-middleware/reducers/authReducer";
-import useWindowDimensions from "../. ./. ./utils/useWindowDimensions";
+} from "../../../redux-saga-middleware/reducers/authReducer";
+import useWindowDimensions from "../../../utils/useWindowDimensions";
 import CreatePassword from "../Auth/CreatePassword";
 import ForgetPassword from "../Auth/ForgetPassword";
 import Login from "../Auth/Login";
 import OTPResetPassword from "../Auth/OTPResetPassword";
 import { default as OTPVerifyAccount } from "../Auth/OTPVerifyAccount";
 import Signup from "../Auth/Signup";
-import "./. ./Auth/Signin/index.scss";
+import "./../Auth/Signin/index.scss";
 
 export default function SimpleDialog(props) {
-const { currentTab, isLoginDialog } = useSelector(
+  const { currentTab, isLoginDialog } = useSelector(
     (state) => state.authReducer
-);
+  );
 
-const dispatch = useDispatch();
-const { width } = useWindowDimensions();
+  const dispatch = useDispatch();
+  const { width } = useWindowDimensions();
 
-const handleClose = () => {
+  const handleClose = () => {
     dispatch(toggleLoginDialog());
     dispatch(clickTab("login"));
-};
+  };
 
-return ReactDOM.createPortal(
+  return ReactDOM.createPortal(
     <>
-     {width < 576 ? (
+      {width < 576 ? (
         <Dialog
-         onCl ose={handleClose}
-         open={isLoginDialog}
-         fullScreen={true}
-         sx={{
+          onClose={handleClose}
+          open={isLoginDialog}
+          fullScreen={true}
+          sx={{
             backgroundColor: "#291e3b",
             height: "100%",
             overflowX: "hidden",
@@ -48,137 +48,137 @@ return ReactDOM.createPortal(
             flexDirection: "row",
             maxWidth: "820px !important",
             zIndex: "1320",
-         }}
+          }}
         >
-         <Box sx={{ height: "100vh" }}>
+          <Box sx={{ height: "100vh" }}>
             <Box
-             sx={{
+              sx={{
                 backgroundColor: "#291e3b",
                 height: "100%",
                 width: "auto",
                 display: "flex",
                 alignItems: "center",
-             }}
-             className="p-2"
+              }}
+              className="p-2"
             >
-             <Box>
+              <Box>
                 <Close
-                 sx={{
+                  sx={{
                     position: "absolute",
                     top: "10px",
                     right: "10px",
                     color: "gray",
                     fontSize: "30px",
-                 }}
-                 onCl ick={handleClose}
+                  }}
+                  onClick={handleClose}
                 />
-             </Box>
-             {currentTab === "login" ? (
+              </Box>
+              {currentTab === "login" ? (
                 <Login />
-             ) : currentTab === "signup" ? (
+              ) : currentTab === "signup" ? (
                 <Signup />
-             ) : currentTab === "forgetPass" ? (
+              ) : currentTab === "forgetPass" ? (
                 <ForgetPassword></ForgetPassword>
-             ) : currentTab === "otpVerifyAccount" ? (
+              ) : currentTab === "otpVerifyAccount" ? (
                 <OTPVerifyAccount></OTPVerifyAccount>
-             ) : currentTab === "otpResetPassword" ? (
+              ) : currentTab === "otpResetPassword" ? (
                 <OTPResetPassword></OTPResetPassword>
-             ) : currentTab === "createPass" ? (
+              ) : currentTab === "createPass" ? (
                 <CreatePassword></CreatePassword>
-             ) : (
+              ) : (
                 <Login></Login>
-             )}
+              )}
             </Box>
-         </Box>
+          </Box>
         </Dialog>
-     ) : (
+      ) : (
         <Dialog
-         onCl ose={handleClose}
-         open={isLoginDialog}
-         maxWidth={"md"}
-         sx={{
+          onClose={handleClose}
+          open={isLoginDialog}
+          maxWidth={"md"}
+          sx={{
             ".css-1t1j96h-MuiPaper-root-MuiDialog-paper": {
-             backgroundColor: "#291e3b",
-             height: "auto",
-             overflowX: "hidden",
-             display: "flex",
-             flexDirection: "row",
-             maxWidth: "820px !important",
-             position: "relative",
+              backgroundColor: "#291e3b",
+              height: "auto",
+              overflowX: "hidden",
+              display: "flex",
+              flexDirection: "row",
+              maxWidth: "820px !important",
+              position: "relative",
             },
-         }}
+          }}
         >
-         <Grid container>
+          <Grid container>
             <Grid item md={6}>
-             <Box
+              <Box
                 sx={{
-                 backgroundColor: "#291e3b",
-                 height: "100%",
-                 width: "auto",
-                 display: "flex",
-                 alignItems: "center",
+                  backgroundColor: "#291e3b",
+                  height: "100%",
+                  width: "auto",
+                  display: "flex",
+                  alignItems: "center",
                 }}
                 className="p-2"
-             >
+              >
                 {currentTab === "login" ? (
-                 <Login />
+                  <Login />
                 ) : currentTab === "signup" ? (
-                 <Signup />
+                  <Signup />
                 ) : currentTab === "forgetPass" ? (
-                 <ForgetPassword></ForgetPassword>
+                  <ForgetPassword></ForgetPassword>
                 ) : currentTab === "otpVerifyAccount" ? (
-                 <OTPVerifyAccount></OTPVerifyAccount>
+                  <OTPVerifyAccount></OTPVerifyAccount>
                 ) : currentTab === "otpResetPassword" ? (
-                 <OTPResetPassword></OTPResetPassword>
+                  <OTPResetPassword></OTPResetPassword>
                 ) : currentTab === "createPass" ? (
-                 <CreatePassword></CreatePassword>
+                  <CreatePassword></CreatePassword>
                 ) : (
-                 <Login></Login>
+                  <Login></Login>
                 )}
-             </Box>
+              </Box>
             </Grid>
             <Grid item md={6}>
-             {width > 992 ? (
+              {width > 992 ? (
                 <Box
-                 sx={{
+                  sx={{
                     backgroundColor: "#19133e",
                     color: "white",
                     height: "100%",
                     position: "relative",
-                 }}
+                  }}
                 >
-                 <img
+                  <img
                     src={
-                     getAppType() === "promote"
+                      getAppType() === "promote"
                         ? sign.bannersignin
                         : images?.signInCrypto
                     }
                     alt="..."
                     width={"100%"}
                     height={"100%"}
-                    st yle={{ backgroundColor: "#3a2b6d" }}
-                 />
-                 <Box
+                    style={{ backgroundColor: "#3a2b6d" }}
+                  />
+                  <Box
                     component={"img"}
                     src={sign.btnBack}
                     sx={{
-                     width: "20px",
-                     height: "20px",
-                     position: "absolute",
-                     zIndex: "100",
-                     top: "20px",
-                     right: "20px",
-                     cursor: "pointer",
+                      width: "20px",
+                      height: "20px",
+                      position: "absolute",
+                      zIndex: "100",
+                      top: "20px",
+                      right: "20px",
+                      cursor: "pointer",
                     }}
-                    onCl ick={handleClose}
-                 ></Box>
+                    onClick={handleClose}
+                  ></Box>
                 </Box>
-             ) : null}
+              ) : null}
             </Grid>
-         </Grid>
+          </Grid>
         </Dialog>
-     )}
+      )}
     </>,
     document.body
-);
+  );
 }
