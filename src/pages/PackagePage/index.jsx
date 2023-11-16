a43b2343d5ec086b779655d9ad47548f70736b81
Riskboy <lamdinh@gadgame.com>
2023-11-16 17:08:42 +0700

    fix: Fix chat plugin

--- src/pages/PackagePage/index.jsx
+++ src/pages/PackagePage/index.jsx
@@ -1,8 +1,16 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import MainLayout from "../../components/MainLayout/MainLayout";
+import { getListPackage } from "../../redux-saga-middleware/reducers/appReducer";
 import { getAppType } from "../../utils/helper";
 import { Package } from "./component";
 
 export default function PackagePage () {
+    const dispatch = useDispatch()
+    useEffect(() => {
+        dispatch(getListPackage())
+    })
+
     return (
         <MainLayout 
             children={
