99278e3fefa2446ad05527cfa9a1ac5f7b21fa8c
thanhdat <thanhdat@gadgame.com>
2023-11-17 16:07:21 +0700

    feat: Fix button device tablet

--- src/pages/PackagePage/component/ListPackage/index.jsx
+++ src/pages/PackagePage/component/ListPackage/index.jsx
@@ -440,7 +440,7 @@ export default function ListPackage(props) {
                   }
                   onClick={handleBuyPackage}
                   style={{
-                    padding: "4px 30px",
+                    padding: "4px 3px",
                     color: "white",
                     background:
                       uPack &&
