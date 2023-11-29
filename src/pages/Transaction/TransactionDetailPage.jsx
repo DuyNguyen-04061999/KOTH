6cc3fee0df254315bd513e5bcb320a58a668f0f1
nqkhanh2003 <nqkhanh2003@gmail.com>
2023-11-29 09:48:53 +0700

    feat: Remove components using web3 package

--- src/pages/Transaction/TransactionDetailPage.jsx
+++ src/pages/Transaction/TransactionDetailPage.jsx
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { useLocation, useParams } from 'react-router-dom'
+import { useLocation, useParams } from 'react-router-dom';
 import _socket from '../../redux-saga-middleware/config/socket';
-import Web3 from "web3"
+// import Web3 from "web3"
 import { useDispatch } from 'react-redux';
-import { showAlert } from '../../redux-saga-middleware/reducers/alertReducer';
 
 export default function TransactionDetailPage() {
     const params = useParams()
@@ -25,87 +24,87 @@ export default function TransactionDetailPage() {
       socket?.emit("checkTransactionId", {  transactionId: id })
     }, [id, socket])
 
-    useEffect(() => {
-        async function sendToken(transaction) {
-          try {
-            let web3 = new Web3(window.ethereum)
-            await window.ethereum.enable();
-            let accounts = await web3.eth.getAccounts()
-            let account = accounts[0]
+    // useEffect(() => {
+    //     async function sendToken(transaction) {
+    //       try {
+    //         let web3 = new Web3(window.ethereum)
+    //         await window.ethereum.enable();
+    //         let accounts = await web3.eth.getAccounts()
+    //         let account = accounts[0]
             
-            if (web3.currentProvider.isConnected()) {
-              console.log("Metamask connected !");
-            } else {
-              console.log("Metamask disconnected !");
-            }
+    //         if (web3.currentProvider.isConnected()) {
+    //           console.log("Metamask connected !");
+    //         } else {
+    //           console.log("Metamask disconnected !");
+    //         }
       
-            socket?.emit("updateDepositTransactionQr", {
-              type: "process",
-              transactionId: id,
-              userId: query?.get("token")
-            })
+    //         socket?.emit("updateDepositTransactionQr", {
+    //           type: "process",
+    //           transactionId: id,
+    //           userId: query?.get("token")
+    //         })
       
-            let contract = new web3.eth.Contract(JSON.parse(transaction?.transactionAbi), transaction?.transactionContract)
-            let depositAmount = (transaction?.transactionQuantity) + "00000000";
+    //         let contract = new web3.eth.Contract(JSON.parse(transaction?.transactionAbi), transaction?.transactionContract)
+    //         let depositAmount = (transaction?.transactionQuantity) + "00000000";
       
-            if(!transaction?.transactionScanQr || transaction?.transactionScanQr) {
-              let result = await contract.methods.transfer(transaction?.transactionWallet, depositAmount).send({ from: account })
+    //         if(!transaction?.transactionScanQr || transaction?.transactionScanQr) {
+    //           let result = await contract.methods.transfer(transaction?.transactionWallet, depositAmount).send({ from: account })
       
-              if(result) {
-                  socket?.emit("updateDepositTransactionQr", {
-                    type: "confirm",
-                    transactionId: id,
-                    tid: id,
-                    txh: result?.transactionHash,
-                    userId: query?.get("token")
-                  })
-                  return result;
-                } else {
-                  socket?.emit("updateDepositTransactionQr", {
-                    type: "error",
-                    transactionId: id,
-                    userId: query?.get("token")
-                  })
-                }
-            }
-              setProcess(false)
-          } catch (error) {
-            socket?.emit("updateDepositTransactionQr", {
-              type: "error",
-              transactionId: id,
-              userId: query?.get("token")
-            })
-            setProcess(false)
-          }
+    //           if(result) {
+    //               socket?.emit("updateDepositTransactionQr", {
+    //                 type: "confirm",
+    //                 transactionId: id,
+    //                 tid: id,
+    //                 txh: result?.transactionHash,
+    //                 userId: query?.get("token")
+    //               })
+    //               return result;
+    //             } else {
+    //               socket?.emit("updateDepositTransactionQr", {
+    //                 type: "error",
+    //                 transactionId: id,
+    //                 userId: query?.get("token")
+    //               })
+    //             }
+    //         }
+    //           setProcess(false)
+    //       } catch (error) {
+    //         socket?.emit("updateDepositTransactionQr", {
+    //           type: "error",
+    //           transactionId: id,
+    //           userId: query?.get("token")
+    //         })
+    //         setProcess(false)
+    //       }
 
-        return false
-      }
+    //     return false
+    //   }
       
-      socket?.on("checkTransactionIdSuccess", async (transaction) => {
-        await sendToken(transaction)
-      })
+    //   socket?.on("checkTransactionIdSuccess", async (transaction) => {
+    //     await sendToken(transaction)
+    //   })
 
-      socket?.on("updateDepositTransactionQrSuccess", async () => {
-        setProcess(false)
-      })
+    //   socket?.on("updateDepositTransactionQrSuccess", async () => {
+    //     setProcess(false)
+    //   })
 
-      socket?.on("error", async (data) => {
-        setProcess(false)
-        dispatch(showAlert("error", data))
-      })
+    //   socket?.on("error", async (data) => {
+    //     setProcess(false)
+    //     dispatch(showAlert("error", data))
+    //   })
 
-      socket?.on("warning", async (data) => {
-        setProcess(false)
-        dispatch(showAlert("warning", data))
-      })
+    //   socket?.on("warning", async (data) => {
+    //     setProcess(false)
+    //     dispatch(showAlert("warning", data))
+    //   })
 
-      return () => {
-        socket?.off("checkTransactionIdSuccess")
-        socket?.off("error")
-        socket?.off("warning")
-        socket?.off("updateDepositTransactionQrSuccess")
-      }
-    }, [socket, query, id, dispatch])
+    //   return () => {
+    //     socket?.off("checkTransactionIdSuccess")
+    //     socket?.off("error")
+    //     socket?.off("warning")
+    //     socket?.off("updateDepositTransactionQrSuccess")
+    //   }
+    // }, [socket, query, id, dispatch])
 
     return (
       <div className='text-white p-2'>
