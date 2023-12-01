58c78891544b80aa658436c254d1e3b100374b95
nqkhanh2003 <nqkhanh2003@gmail.com>
2023-12-01 11:13:50 +0700

    feat: Added double day pop up

--- src/components/Dialog/DoubleDayPack/index.jsx
+++ src/components/Dialog/DoubleDayPack/index.jsx
@@ -1,37 +1,97 @@
-import { Box, Dialog } from '@mui/material';
-import React from 'react';
+import { Box, Dialog } from "@mui/material";
+import React from "react";
 import ReactDOM from "react-dom";
-import { images } from '../../../utils/images';
-import useWindowDimensions from '../../../utils/useWindowDimensions';
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router";
+import { closeDoubleDayDialog } from "../../../redux-saga-middleware/reducers/appReducer";
+import { images } from "../../../utils/images";
+import useWindowDimensions from "../../../utils/useWindowDimensions";
 
 const DoubleDayPackDialog = () => {
   const { width } = useWindowDimensions();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { showDoubleDayDialog, randomRender } = useSelector(
+    (state) => state.appReducer
+  );
+
+  const handleClose = () => {
+    dispatch(closeDoubleDayDialog());
+  };
   return ReactDOM.createPortal(
-<>
+    <>
       {width < 576 ? (
         <div>
           <Dialog
-            open={false}
+            open={showDoubleDayDialog && randomRender}
+            onClose={handleClose}
             aria-labelledby="alert-dialog-title"
             aria-describedby="alert-dialog-description"
           >
-            <Box component={"img"} src={images.doubleDayPackBannerMB}></Box>
+            <Box
+              sx={{ position: "relative", cursor: "pointer" }}
+            >
+              <Box
+                sx={{ width: "100%" }}
+                component={"img"}
+                src={images.doubleDayPackBannerMB}
+                onClick={() => {
+                  handleClose();
+                  navigate("/packages");
+                }}
+              ></Box>
+              <Box
+                onClick={handleClose}
+                sx={{
+                  position: "absolute",
+                  top: "20px",
+                  right: "20px",
+                  cursor: "pointer",
+                }}
+                component={"img"}
+                src={images.btn_back}
+              ></Box>
+            </Box>
           </Dialog>
         </div>
       ) : (
         <div>
           <Dialog
-            open={false}
+            open={showDoubleDayDialog && randomRender}
+            onClose={handleClose}
             aria-labelledby="alert-dialog-title"
             aria-describedby="alert-dialog-description"
           >
-            <Box component={"img"} src={images.doubleDayPackBanner}></Box>
+            <Box
+              sx={{ position: "relative", cursor: "pointer" }}
+            >
+              <Box
+                sx={{ width: "100%" }}
+                component={"img"}
+                src={images.doubleDayPackBanner}
+                onClick={() => {
+                  handleClose();
+                  navigate("/packages");
+                }}
+              ></Box>
+              <Box
+                onClick={handleClose}
+                sx={{
+                  position: "absolute",
+                  top: "20px",
+                  right: "20px",
+                  cursor: "pointer",
+                }}
+                component={"img"}
+                src={images.btn_back}
+              ></Box>
+            </Box>
           </Dialog>
         </div>
       )}
     </>,
-      document.body
-    );
-}
+    document.body
+  );
+};
 
-export default DoubleDayPackDialog
+export default DoubleDayPackDialog;
