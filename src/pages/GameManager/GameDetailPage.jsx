b2816369ec356d95d821a17549e1a71994dfcad7
Riskboy <lamdinh@gadgame.com>
2023-12-14 13:16:29 +0700

    fix: Update test game page

--- src/pages/GameManager/GameDetailPage.jsx
+++ src/pages/GameManager/GameDetailPage.jsx
@@ -1,9 +1,12 @@
-import React, { Fragment, useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
 import axios from "axios";
-import UnityGameComponent from "../../components/GameManager/UnityGameComponent";
+import React, { Fragment, useEffect, useState } from "react";
+import { FullScreen, useFullScreenHandle } from "react-full-screen";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import DeviceOrientation, { Orientation } from 'react-screen-orientation';
 
-export default function GameDetailPage() {
+export default function GameDetailPage(props) {
   const { id } = useParams();
   const [fetchGame, setFetchGame] = useState(true);
   const [game, setGame] = useState(null);
@@ -22,13 +25,165 @@ export default function GameDetailPage() {
         });
   });
 
+  const screen = useFullScreenHandle();
+  const { device } = useSelector((state) => state.deviceReducer);
+  const { orientation } = useSelector((state) => state.gameReducer);
+  const [isFullScreen] = useState(true);
+  const getMobileOS = () => {
+    const ua = navigator.userAgent;
+    if (/android/i.test(ua)) {
+      return "Android";
+    } else if (
+      /iPad|iPhone|iPod/.test(ua) ||
+      (navigator.platform === "MacIntel" && navigator.maxTouchPoints > 1)
+    ) {
+      return "iOS";
+    }
+    return "Window";
+  };
+  const os = getMobileOS();
+
   return (
     <Fragment>
       {!fetchGame && game ? (
-        <UnityGameComponent
-          gameScreenType={game?.gameScreenType}
-          GameFiles={game?.GameFiles}
-        />
+        <Box
+          sx={
+            device === "Mobile"
+              ? game?.gameScreenType
+                ? orientation === "portrait"
+                  ? {
+                      transform: " rotate(-90deg)",
+                      transformOrigin: "left top",
+                      width: "100vh",
+                      height: "100vw",
+                      overflowX: "hidden",
+                      position: "fixed",
+                      top: "100vh",
+                      left: "0px",
+                    }
+                  : {}
+                : orientation === "landscape"
+                ? {
+                    transform: " rotate(-90deg)",
+                    transformOrigin: "left top",
+                    width: "100vh",
+                    height: "100vw",
+                    overflowX: "hidden",
+                    position: "fixed",
+                    top: "100vh",
+                    left: "0px",
+                  }
+                : {}
+              : {}}
+      >
+        <Box
+          sx={{
+            width: "100%",
+            height: "auto",
+            display: "flex",
+            paddingTop: "50px",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "column",
+          }}
+        >
+          <Box
+            sx={{
+              width: isFullScreen
+                ? "100%"
+                : device === "Mobile" || device === "Tablet"
+                ? "100%"
+                : "80%",
+              height: isFullScreen ? "100%" : "auto",
+              position: isFullScreen ? "fixed" : "relative",
+              backgroundColor: isFullScreen ? "black" : "none",
+              top: isFullScreen ? "0px" : "none",
+              left: isFullScreen ? "0px" : "none",
+              zIndex: isFullScreen ? "5005" : "none",
+            }}
+          >
+            <Box
+              sx={{
+                position: "relative",
+                height: "800px",
+              }}
+            >
+              {" "}
+            
+              {device && os && device === "Mobile" && os === "Android" ? (
+                <DeviceOrientation lockOrientation={'landscape'}>
+                <Orientation orientation='landscape'>
+                <FullScreen handle={screen}>
+                <iframe
+                    allow="fullscreen"
+                    style={
+                      device === "Desktop"
+                        ? {
+                            width: "100%",
+                            height: "800px",
+                            background: "black",
+                            display: "block",
+                          }
+                        : {
+                            position: "fixed",
+                            top: "0",
+                            left: "0",
+                            bottom: "0",
+                            right: "0",
+                            width: "100%",
+                            height: "100%",
+                            border: "none",
+                            margin: "0",
+                            padding: "0",
+                            overflow: "hidden",
+                            zIndex: "999999",
+                            display: "block",
+                          }
+                    }
+                    title="Playgame"
+                    src={
+                      `${window.location.origin}/game/${id}`
+                    }
+                  ></iframe>
+                </FullScreen>
+                  </Orientation>
+                </DeviceOrientation>
+              ) : (
+                <iframe
+                    allow="fullscreen"
+                    style={
+                      device === "Desktop"
+                        ? {
+                            width: "100%",
+                            height: "800px",
+                            background: "black",
+                            display: "block",
+                          }
+                        : {
+                            position: "fixed",
+                            top: "0",
+                            left: "0",
+                            bottom: "0",
+                            right: "0",
+                            width: "100%",
+                            height: "100%",
+                            border: "none",
+                            margin: "0",
+                            padding: "0",
+                            overflow: "hidden",
+                            zIndex: "999999",
+                            display: "block",
+                          }
+                    }
+                    title="Playgame"
+                    src={
+                      `${window.location.origin}/game/${id}`
+                    }
+                  ></iframe> )}
+            </Box>
+          </Box>
+        </Box>
+      </Box>
       ) : (
         <>Loading</>
       )}
