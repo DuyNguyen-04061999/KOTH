f9593b2c947ce7d022eb978d85be56d0d3b10892
thanhduy <btduy12.11lvl@gmail.com>
2023-12-19 15:37:20 +0700

    feat: Integrate PePe mascot to Sign In

--- src/utils/validatePhoneNumber.js
+++ src/utils/validatePhoneNumber.js
@@ -1,8 +1,7 @@
 export const validatePhoneNumber = (phone) => {
-    // Define the regex pattern
-    const pattern = /^\(?([0-9]{3})\)?[-.●]?([0-9]{3})[-.●]?([0-9]{4})$/;
+  // Define the regex pattern
+  const pattern = /^\(?([0-9]{3})\)?[-.●]?([0-9]{3})[-.●]?([0-9]{4})$/;
 
-    // Use test method to check if the phone number matches the pattern
-    return pattern.test(phone);
-  };
-  
+  // Use test method to check if the phone number matches the pattern
+  return pattern.test(phone);
+};
