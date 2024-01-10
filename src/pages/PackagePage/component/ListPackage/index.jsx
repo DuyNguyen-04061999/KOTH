29dccdac7db4f70600c24b3ef42381542dc5ae8c
Riskboy <lamdinh@gadgame.com>
2024-01-10 10:19:59 +0700

    fix: Update packages

--- src/pages/PackagePage/component/ListPackage/index.jsx
+++ src/pages/PackagePage/component/ListPackage/index.jsx
@@ -601,7 +601,7 @@ export default function ListPackage(props) {
             marginLeft: "8px",
             marginRight: "8px",
             height: "fit-content",
-            minWidth: "315px",
+            minWidth: width > 576 && width < 1200 ? "100%" : "315px",
             marginBottom: "20px",
             maxWidth: width > 576 && width < 1200 ? "315px" : "unset",
           }}
