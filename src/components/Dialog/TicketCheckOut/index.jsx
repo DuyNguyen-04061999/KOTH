13786a9bd6974fdc704570f3a6407235999eb9b2
thanhdat <thanhdat@gadgame.com>
2024-01-17 11:28:57 +0700

    fix: default value checked pack subscription

--- src/components/Dialog/TicketCheckOut/index.jsx
+++ src/components/Dialog/TicketCheckOut/index.jsx
@@ -37,7 +37,7 @@ export default function TicketCheckOut() {
   const [feeMoney, setFeeMoney] = useState(0);
   const [totalMoney, setTotalMoney] = useState(0);
   const [sl, setSl] = useState(1);
-  const [autoRecurring, setAutoRecurring] = useState(false);
+  const [autoRecurring, setAutoRecurring] = useState(true);
 
   useEffect(() => {
     if (typePayment === "skrill") {
