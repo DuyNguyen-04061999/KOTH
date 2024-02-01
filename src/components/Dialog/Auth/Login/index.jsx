c25a3911c5450639daa6fdc258c689d1511a76ca
Riskboy <lamdinh@gadgame.com>
2024-02-01 15:25:45 +0700

    fix: Update remember me

--- src/components/Dialog/Auth/Login/index.jsx
+++ src/components/Dialog/Auth/Login/index.jsx
@@ -1,6 +1,6 @@
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
-import { Box, FormControl, Input, Typography } from "@mui/material";
+import { Box, FormControl, Input, Typography, Checkbox } from "@mui/material";
 import { useRive, useStateMachineInput } from "@rive-app/react-canvas";
 import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
@@ -17,6 +17,7 @@ import { validatePhoneNumber } from "../../../../utils/validatePhoneNumber";
 import { validateEmail } from "../../../../utils/validationEmail";
 import AnimButton from "../../../AnimButton";
 import ReactGA from "react-ga4";
+import { styled } from '@mui/material/styles';
 
 const Login = () => {
   const dispatch = useDispatch();
@@ -64,6 +65,7 @@ const Login = () => {
           loginReady({
             email: username,
             password: password,
+            remember: remember
           })
         );
       } else if (validatePhoneNumber(username)) {
@@ -71,6 +73,7 @@ const Login = () => {
           loginReady({
             phone: username,
             password: password,
+            remember: remember
           })
         );
       }
@@ -148,6 +151,74 @@ const Login = () => {
 
   const { orientation } = useSelector((state) => state.gameReducer);
   const { device } = useSelector((state) => state.deviceReducer);
+
+  const [remember, setRemember] = useState(false)
+
+
+  const BpIcon = styled('span')(({ theme }) => ({
+    borderRadius: 3,
+    width: 16,
+    height: 16,
+    boxShadow:
+      theme.palette.mode === 'dark'
+        ? '0 0 0 1px rgb(16 22 26 / 40%)'
+        : 'inset 0 0 0 1px rgba(16,22,26,.2), inset 0 -1px 0 rgba(16,22,26,.1)',
+    backgroundColor: theme.palette.mode === 'dark' ? '#394b59' : '#f5f8fa',
+    backgroundImage:
+      theme.palette.mode === 'dark'
+        ? 'linear-gradient(180deg,hsla(0,0%,100%,.05),hsla(0,0%,100%,0))'
+        : 'linear-gradient(180deg,hsla(0,0%,100%,.8),hsla(0,0%,100%,0))',
+    '.Mui-focusVisible &': {
+      outline: '2px auto rgba(19,124,189,.6)',
+      outlineOffset: 2,
+    },
+    'input:hover ~ &': {
+      backgroundColor: theme.palette.mode === 'dark' ? '#30404d' : '#ebf1f5',
+    },
+    'input:disabled ~ &': {
+      boxShadow: 'none',
+      background:
+        theme.palette.mode === 'dark' ? 'rgba(57,75,89,.5)' : 'rgba(206,217,224,.5)',
+    },
+  }));
+  
+  const BpCheckedIcon = styled(BpIcon)({
+    backgroundColor: '#7848ED',
+    backgroundImage: 'linear-gradient(180deg,hsla(0,0%,100%,.1),hsla(0,0%,100%,0))',
+    '&::before': {
+      display: 'block',
+      width: 16,
+      height: 16,
+      backgroundImage:
+        "url(\"data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'%3E%3Cpath" +
+        " fill-rule='evenodd' clip-rule='evenodd' d='M12 5c-.28 0-.53.11-.71.29L7 9.59l-2.29-2.3a1.003 " +
+        "1.003 0 00-1.42 1.42l3 3c.18.18.43.29.71.29s.53-.11.71-.29l5-5A1.003 1.003 0 0012 5z' fill='%23fff'/%3E%3C/svg%3E\")",
+      content: '""',
+    },
+    'input:hover ~ &': {
+      backgroundColor: '#106ba3',
+    },
+  });
+  
+  // Inspired by blueprintjs
+  function BpCheckbox(props) {
+    const { checked,  onChange } = props
+    return (
+      <Checkbox
+        sx={{
+          '&:hover': { bgcolor: 'transparent' },
+        }}
+        disableRipple
+        color="default"
+        checkedIcon={<BpCheckedIcon />}
+        icon={<BpIcon />}
+        checked={checked}
+        onChange={() => onChange()}
+        inputProps={{ 'aria-label': 'Checkbox demo' }}
+        {...props}
+      />
+    );
+  }
 
   return (
     <Box
@@ -302,10 +373,13 @@ const Login = () => {
           sx={{
             display: "flex",
             alignItems: "center",
-            justifyContent: "flex-end",
+            justifyContent: "space-between",
             margin: "12px 0",
           }}
-        >
+        ><Box className="text-white">
+          <BpCheckbox onChange={() => setRemember(!remember)} checked={remember}/>
+        Remember me?
+      </Box>
           <Typography
             sx={{
               color: "#FF9F38",
