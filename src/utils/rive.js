7c94c117e00ff160ce4ecd5ee8c5881e29cb1f5e
thanhduy <btduy12.11lvl@gmail.com>
2024-02-05 15:23:35 +0700

    feat: Change rive mascot pepe

--- src/utils/rive.js
+++ src/utils/rive.js
@@ -1,5 +1,5 @@
 import christmas_pepe from "../assets/rive/christmas_pepe.riv";
-import casual_pepe from "../assets/rive/casual_pepe v1.0.riv";
+import casual_pepe from "../assets/rive/casual_pepe_v1.1.riv";
 export const riveFile = {
   christmas_pepe: christmas_pepe,
   casual_pepe: casual_pepe,
