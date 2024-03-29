9f6ae5052824f15c81b84a374db5574e5bfbf04e
quockhanh <quockhanh@gadgame.com>
2024-03-29 15:08:33 +0700

    fix: Change buy pack flow, cancel renew after buy pack

--- src/redux-saga-middleware/services/checkoutService.js
+++ src/redux-saga-middleware/services/checkoutService.js
@@ -1,104 +1,102 @@
-import { PROMOTION_API } from "../axios/promotionApi";
+import {PROMOTION_API} from "../axios/promotionApi";
 
 class CheckoutService {
-  async getCheckout(dataRequest) {
-    const token = localStorage.getItem("token");
-    const res = await PROMOTION_API.post(
-      "/api/payments/paypal/buy-package",
-      {
-        ...dataRequest,
-      },
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: token,
-          authorization: token,
-          "x-access-refactor-token": token,
-        },
-      }
-    );
-    return res;
-  }
+    async getCheckout(dataRequest) {
+        const token = localStorage.getItem("token");
+        const res = await PROMOTION_API.post(
+            "/api/payments/paypal/buy-package",
+            {
+                ...dataRequest,
+            },
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: token,
+                    authorization: token,
+                    "x-access-refactor-token": token,
+                },
+            }
+        );
+        return res;
+    }
 
-  async getCheckoutSuccess(dataRequest) {
-    const token = localStorage.getItem("token");
-    const res = await PROMOTION_API.post(
-      "/api/payments/paypal/buy-package/success",
+    async getCheckoutSuccess(dataRequest) {
+        const token = localStorage.getItem("token");
+        const res = await PROMOTION_API.post(
+            "/api/payments/paypal/buy-package/success",
 
-      {
-        ...dataRequest,
-      },
+            {
+                ...dataRequest,
+            },
 
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: token,
-          authorization: token,
-          "x-access-refactor-token": token,
-        },
-      }
-    );
-    return res;
-  }
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: token,
+                    authorization: token,
+                    "x-access-refactor-token": token,
+                },
+            }
+        );
+        return res;
+    }
 
-  // async getCheckOutAutoCancel(dataRequest) {
+    // async getCheckOutAutoCancel(dataRequest) {
 
-  //   const token = localStorage.getItem("token");
-  //   const res = await PROMOTION_API.post(
-  //     "/api/payments/paypal/auto/cancel",
-  //     {
-  //       packageId: Number(JSON.parse(packageRenewChanged?.id)),
-  //     },
-  //     {
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //         Authorization: token,
-  //         authorization: token,
-  //         "x-access-refactor-token": token,
-  //       },
-  //     }
-  //   );
-  //   return res
-  // }
+    //   const token = localStorage.getItem("token");
+    //   const res = await PROMOTION_API.post(
+    //     "/api/payments/paypal/auto/cancel",
+    //     {
+    //       packageId: Number(JSON.parse(packageRenewChanged?.id)),
+    //     },
+    //     {
+    //       headers: {
+    //         "Content-Type": "application/json",
+    //         Authorization: token,
+    //         authorization: token,
+    //         "x-access-refactor-token": token,
+    //       },
+    //     }
+    //   );
+    //   return res
+    // }
 
-  async cancelCurentPackage(dataRequest) {
-    const packageRenewChanged = localStorage.getItem("previousPack")
-    const token = localStorage.getItem("token");
-    const res = await PROMOTION_API.post(
-      "/api/payments/paypal/auto/cancel",
-      {
-        packageId: Number(JSON.parse(packageRenewChanged)?.id),
-      },
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: token,
-          authorization: token,
-          "x-access-refactor-token": token,
-        },
-      }
-    );
-    return res
-  }
+    async cancelCurentPackage(dataRequest) {
+        const token = localStorage.getItem("token");
+        return await PROMOTION_API.post(
+            "/api/payments/paypal/auto/cancel",
+            {
+                ...dataRequest
+            },
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: token,
+                    authorization: token,
+                    "x-access-refactor-token": token,
+                },
+            }
+        )
+    }
 
-  async getCheckoutCancel(dataRequest) {
-    const token = localStorage.getItem("token");
-    const res = await PROMOTION_API.post(
-      "/api/payments/paypal/buy-package/cancel",
-      {
-        ...dataRequest,
-      },
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: token,
-          authorization: token,
-          "x-access-refactor-token": token,
-        },
-      }
-    );
-    return res;
-  }
+    async getCheckoutCancel(dataRequest) {
+        const token = localStorage.getItem("token");
+        const res = await PROMOTION_API.post(
+            "/api/payments/paypal/buy-package/cancel",
+            {
+                ...dataRequest,
+            },
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: token,
+                    authorization: token,
+                    "x-access-refactor-token": token,
+                },
+            }
+        );
+        return res;
+    }
 }
 
 export default CheckoutService;
