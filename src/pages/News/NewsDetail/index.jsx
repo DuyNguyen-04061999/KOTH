e1196b2b0f99ca77179a2c836def063f6319fd2b
quockhanh <quockhanh@gadgame.com>
2024-04-05 18:42:59 +0700

    style: Styled page news layout

--- src/pages/News/NewsDetail/index.jsx
+++ src/pages/News/NewsDetail/index.jsx
@@ -1,91 +1,84 @@
-import React, { useEffect, Suspense, useState } from "react";
-import { Container, Typography, Box, Skeleton } from "@mui/material";
+import React, {useEffect, Suspense, useState} from "react";
+import {Container, Typography, Box, Skeleton} from "@mui/material";
 
-import { useDispatch, useSelector } from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 
-import { Routes, Route, useParams } from "react-router-dom";
+import {Routes, Route, useParams} from "react-router-dom";
 import MainLayout from "../../../components/MainLayout/MainLayout";
-import { getListNewsDetail } from "../../../redux-saga-middleware/reducers/news";
-import { images } from "../../../utils/images";
+import {getListNewsDetail} from "../../../redux-saga-middleware/reducers/news";
 import ParagraphLoading from "../../../components/LoadingComponent/ParagraphLoading";
 import NewFooter from "../../NewFooter";
+import "react-quill/dist/quill.core.css";
 
 export default function NewsDetail() {
-  const { listNewDetail, isNewsDetail } = useSelector(
-    (state) => state.newsReducer
-  );
-  const { device } = useSelector((state) => state.deviceReducer);
-  const [dataNewsDetail, setDataNewsDetail] = useState({});
-  const { id } = useParams();
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(
-      getListNewsDetail({
-        id: id,
-      })
+    const {listNewDetail, isNewsDetail} = useSelector(
+        (state) => state.newsReducer
     );
-  }, [id]);
+    const {device} = useSelector((state) => state.deviceReducer);
+    const [dataNewsDetail, setDataNewsDetail] = useState({});
+    const {id} = useParams();
+    const dispatch = useDispatch();
 
-  useEffect(() => {
-    if (listNewDetail) {
-      setDataNewsDetail(listNewDetail);
-    }
-  }, [listNewDetail]);
+    useEffect(() => {
+        dispatch(
+            getListNewsDetail({
+                id: id,
+            })
+        );
+    }, [id]);
 
-  return (
-    <>
-      <MainLayout
-        children={
-          <Container maxWidth="lg">
-            {/* <Box className="image-title" sx={{ paddingTop: device === "Mobile" ? "40px" : "60px" }}>
-              <Box
-                component={"img"}
-                src={images.BannerHomePageDesktop}
-                alt="banner"
-                sx={{width:" 100%", height:"100%"}}
-              ></Box>
-            </Box> */}
-            {isNewsDetail ? (
-              <Box className="pt-4">
-                <ParagraphLoading />
-              </Box>
-            ) : (
-              <Box className="body-desc">
-                <Box
-                  className="desc-title"
-                  sx={{
-                    padding: device === "Mobile" ? "20px 0" : "40px 0",
-                  }}
-                >
-                  <Typography
-                    sx={{
-                      color: "white",
-                      fontSize: device === "Mobile" ? "18px" : "26px",
-                      textAlign: "left",
-                    }}
-                  >
-                    {dataNewsDetail?.title}
-                  </Typography>
-                </Box>
-                <Box
-                  dangerouslySetInnerHTML={{ __html: dataNewsDetail?.body }}
-                  className="desc-body"
-                  sx={{
-                    color: "white",
-                    fontSize: device === "Mobile" ? "14px" : "18px",
-                    textAlign: "left",
-                  }}
-                ></Box>
-              </Box>
-            )}
-            <Box className="footer">
-              <Suspense fallback="loading..." children={<NewFooter />} />
-            </Box>
-          </Container>
+    useEffect(() => {
+        if (listNewDetail) {
+            setDataNewsDetail(listNewDetail);
         }
-        type="Home"
-      />
-    </>
-  );
+    }, [listNewDetail]);
+
+    return (
+        <>
+            <MainLayout
+                children={
+                    <Container maxWidth="lg">
+                        {isNewsDetail ? (
+                            <Box className="pt-4">
+                                <ParagraphLoading/>
+                            </Box>
+                        ) : (
+                            <Box className="body-desc">
+                                <Box
+                                    className="desc-title"
+                                    sx={{
+                                        padding: device === "Mobile" ? "20px 0" : "40px 0",
+                                    }}
+                                >
+                                    <Typography
+                                        sx={{
+                                            color: "white",
+                                            fontSize: device === "Mobile" ? "24px" : "36px",
+                                            textAlign: "left",
+                                            fontWeight: 800
+                                        }}
+                                    >
+                                        {dataNewsDetail?.title}
+                                    </Typography>
+                                </Box>
+                                <Box
+                                    className="view ql-editor desc-body"
+                                    dangerouslySetInnerHTML={{__html: dataNewsDetail?.body}}
+                                    sx={{
+                                        color: "white",
+                                        fontSize: device === "Mobile" ? "13px" : "15px",
+                                        textAlign: "left",
+                                    }}
+                                ></Box>
+                            </Box>
+                        )}
+                        <Box className="footer">
+                            <Suspense fallback="loading..." children={<NewFooter/>}/>
+                        </Box>
+                    </Container>
+                }
+                type="Home"
+            />
+        </>
+    );
 }
