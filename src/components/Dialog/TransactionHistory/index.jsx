e60b3a9b29498c4142cf56f5dd9b1f69818dee5b
thanhduy <btduy12.11lvl@gmail.com>
2024-04-11 12:01:56 +0700

    fix: change size of transaction history table

--- src/components/Dialog/TransactionHistory/index.jsx
+++ src/components/Dialog/TransactionHistory/index.jsx
@@ -77,7 +77,7 @@ export default function TransactionHistory() {
               alignItems: "center",
             }}
           >
-            {params?.row?.transactionPackage?.packagePrice}
+            {"$" + params?.row?.transactionPackage?.packagePrice}
           </Box>
         );
       },
@@ -343,6 +343,7 @@ export default function TransactionHistory() {
           padding: "20px",
           boxSizing: "border-box",
           height: device !== "Desktop" ? "100%" : "800px",
+          overflowY: "auto",
         }}
       >
         {device === "Desktop" ? (
@@ -381,6 +382,7 @@ export default function TransactionHistory() {
               width: "100%",
               display: "flex",
               alignItems: "center",
+              zIndex: "10",
             }}
           >
             <Box
@@ -402,9 +404,10 @@ export default function TransactionHistory() {
         )}{" "}
         <Box
           sx={{
-            height: device === "Tablet" ? "auto" : "auto",
+            height: transactionList?.length > 0 ? "auto" : "70vh",
             width: "100%",
             marginTop: "25px",
+            minHeight: "50vh !important",
           }}
         >
           <DataGrid
